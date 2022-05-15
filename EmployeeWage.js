1621bb76448a2bfd87cd95e744dc017a08c0dd97
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-16 01:10:48 +0530

    added UC3: Refactored the code to write a function to get work hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,20 +6,22 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            console.log("Part-Time Employee is Present.");
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            console.log("Full-Time Employee is Present.");
+            return FULL_TIME_HOURS;
+        default:
+            console.log("Employee is Absent.");
+            return 0;
+    }
+}
+
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck){
-    case IS_PART_TIME:
-        console.log("Part-Time Employee is Present.");
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        console.log("Full-Time Employee is Present.");
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        console.log("Employee is Absent.");
-        empHrs = 0;
-}
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log ("Employee Wage: " + empWage);
