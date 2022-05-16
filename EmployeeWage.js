6a64e08277ec97b80aca981ccbdb42e2bbfd674d
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-16 13:57:09 +0530

    added UC4: Ability to calculate wages for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,23 +5,24 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
-function getWorkingHours(empCheck){
-    switch(empCheck){
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
         case IS_PART_TIME:
-            console.log("Part-Time Employee is Present.");
             return PART_TIME_HOURS;
         case IS_FULL_TIME:
-            console.log("Full-Time Employee is Present.");
             return FULL_TIME_HOURS;
         default:
-            console.log("Employee is Absent.");
             return 0;
     }
 }
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log ("Employee Wage: " + empWage);
+let TotalEmpHrs = 0;
+for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    TotalEmpHrs += empHrs;
+}
+let empWage = TotalEmpHrs * WAGE_PER_HOUR;
+console.log("Monthly Employee Wage: " + empWage + "\nTotal Hrs worked in the resp month: " + TotalEmpHrs);
