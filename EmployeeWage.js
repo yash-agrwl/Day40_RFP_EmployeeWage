83aaed1ae801c37ee8134bd0425b1934390e2290
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-16 14:20:32 +0530

    added UC5: Ability to calculate wages till given max limits is reached for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,7 +5,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -18,11 +19,12 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let TotalEmpHrs = 0;
-for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    TotalEmpHrs += empHrs;
+    totalEmpHrs += getWorkingHours(empCheck);
+    totalWorkingDays++;
 }
-let empWage = TotalEmpHrs * WAGE_PER_HOUR;
-console.log("Monthly Employee Wage: " + empWage + "\nTotal Hrs worked in the resp month: " + TotalEmpHrs);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Monthly Employee Wage: " + empWage + "\nTotal Days Worked: " + totalWorkingDays + "\nTotal Hrs worked: " + totalEmpHrs);
